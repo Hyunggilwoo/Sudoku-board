69e5dece7a20d69125316363a9386430f142ddeb
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-06-08 22:12:09 -0700

    Fixed the problem in main to scan identical input to the output 2 dimensional array.

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+/*----------------------------------------------------------------------
+ *                              Limitations
+ * 1. Assumes that the sudoku will be size of the same index in row and in column.
+ * 2. I am not sure how my default constructor will present. 
+ * 			Will it contain zeros, probably.
+ * 3. Because it is a public sudoku, it is immune to being editted at each index.
+ */ 
 import java.io.*;
 import java.util.*;
 public class Main{
 	public static void main(String[]args){
 		try{
-			// count number of students
+			// count number of rows
 			/*Why count the number of students, because it creates an index of array
 			 *based on the number of integers it counted from the text.  
 			*/
@@ -20,13 +27,24 @@ public class Main{
 			
 			/* Constructing a 2 dimensional array with the size of count.
 			 * The array now has a size of the count ( 9 by 9)
+			 * June 8, 21:48
+			 * I am tweaking on how the scanned value from parseLine can be stored 
+			 * at the index of i.
+			 * 
 			 * 
 			 */ 
-			int [][] directory = new int[count][count];
+			int [][] directory = new int [count][count];
 			scan = new Scanner(sudoku1); // reset the scanner called scan
-			for(int i =0;i< count;i++){
-				for(int j= 0;j<count;j++){
-				System.out.print(directory[i][j]);
+			for(int row =0;row < count;row++){ // Outer loop moves the scanner from top row to bottom row.
+				
+				/*  
+				 * 
+				 */ 
+				
+				for(int col= 0;col <count;col++){ // Inner loop moves the scanner from the beginning of index to the end of the index
+					Scanner parseLine = new Scanner(scan.next());
+					directory[row][col] = parseLine.nextInt();
+				System.out.print(directory[row][col]);
 				}
 				System.out.println();
 				
@@ -50,11 +68,11 @@ public class Main{
 			//System.out.println(directory[0].getFamily());
 			//System.out.println(directory[0].getYear());
 			
-			//// try and fail to tamper with grades
-			//String[] newGrades = directory[0].getGrades();
-			//newGrades[0] = "100";newGrades[1] = "100";newGrades[2] = "100";newGrades[3] = "100";
+			// try and fail to tamper with grades
+			String[] newGrades = directory[0].getGrades();
+			newGrades[0] = "100";newGrades[1] = "100";newGrades[2] = "100";newGrades[3] = "100";
 			
-			//System.out.println(Arrays.toString(directory[0].getGrades()));
+			System.out.println(Arrays.toString(directory[0].getGrades()));
 			
 			
 		}catch(FileNotFoundException fnfe){
